73dd64a3f69a320a5caae6359537b99928dc3593
Malavika <malavikaajit9@gmail.com>
2022-07-05 10:46:21 +0530

    added :dashboard embedding

--- index.js
+++ index.js
@@ -364,7 +364,7 @@ app.get("/", (req, res) => {
       //let url = "https://ketaki-desale.app.bipp.io/embed/68cc77a6-fc72-497d-bbb9-0b1ffe01e579?id=707b84b7-7a5c-47a5-9847-a163319daa95&cid=f570dc6573a6478cb954e82fa603ae97.ketaki-desale.app.bipp.io&secret=MNTGHrqSnWBQ0zonnyfphbJn3!mFM!@RP!davQJi!lXkUFEz";
       //let url = "https://ketaki-desale.app.bipp.io/embed/443c2551-937f-44c6-8252-88f3d4739ebc?id=a8c75ef8-d90a-404f-a5de-5139952513ab&cid=16cc46c2c9054fdeac09ea1f6385b380.ketaki-desale.app.bipp.io&secret=r6hXP@nwPUmDFeRO9R@b25dyrfTIv65mOsVPPKEtPJRQncPF";    
       let url = "https://ketaki-desale.app.bipp.io/embed/9c3cd476-1cb8-4ef9-8b6d-cd8c1f095103?id=a8c75ef8-d90a-404f-a5de-5139952513ab&cid=16cc46c2c9054fdeac09ea1f6385b380.ketaki-desale.app.bipp.io&secret=r6hXP@nwPUmDFeRO9R@b25dyrfTIv65mOsVPPKEtPJRQncPF";
-      let config = { id : 'container', width: "70%",height: height, style: 'border:none; padding-left:305px' }
+      let config = { id : 'container', width: "100%",height: height, style: 'border:none; padding-left:305px' }
       
       let bipp = new Bipp();
       bipp.load(url, config);
